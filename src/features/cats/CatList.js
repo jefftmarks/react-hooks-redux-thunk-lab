024e8ef1b0a1d564d6c5253a2fd77cfe4cbeb160
Jeff Marks <jeff.t.marks@gmail.com>
2022-09-25 12:07:25 -0400

    commit tree dcc9f501de6a6e12940fa5c08cc1c4495a108322

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,17 @@
-// write your CatList component here
+import React from "react";
+import "./CatList.css";
+
+function CatList({ catPics }) {
+
+	const renderedCatPics = catPics.map((catPic) => (
+		<img src={catPic.url} alt="cat" key={catPic.id} />
+	))
+
+	return (
+		<div className="cat-container">
+			{renderedCatPics}
+		</div>
+	)
+}
+
+export default CatList;
